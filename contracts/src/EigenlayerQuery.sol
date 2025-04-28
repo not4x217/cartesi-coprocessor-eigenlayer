2533e43aec42eadbfd10add11c23302f6745b3c8
not4x217 <6069441+not4x217@users.noreply.github.com>
2025-04-28 10:29:20 +0300

    update contracts

---
+++ contracts/src/EigenlayerQuery.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "../lib/coprocessor-base-contract/src/CoprocessorAdapter.sol";
+
+contract EigenlayerQuery is CoprocessorAdapter {
+    mapping(bytes32 => bool) public queriesInProgress;
+    mapping(address => mapping(address => mapping(bytes32 => uint256))) public operatorBalances;
+    
+    constructor(address _taskIssuerAddress, bytes32 _machineHash)
+        CoprocessorAdapter(_taskIssuerAddress, _machineHash)
+    {}
+
+    function queryOperatorBalance(address operator, address erc20) external {
+        bytes32 blockHash = blockhash(block.number - 1);
+        bytes memory payload = abi.encode(blockHash, operator, erc20);
+        bytes32 payloadHash = keccak256(payload);  
+        
+        require(!queriesInProgress[payloadHash], "query is already in progress");
+        queriesInProgress[payloadHash] = true;
+        
+        callCoprocessor(payload);
+    }
+
+    function handleNotice(bytes32 payloadHash, bytes memory notice) internal override {
+        require(queriesInProgress[payloadHash], "no query in progress for received notice");
+        
+        (bytes32 blockHash, address operator, address erc20, uint256 balance) = abi.decode(
+            notice,
+            (bytes32, address, address, uint256 )
+        );
+
+        delete queriesInProgress[payloadHash];
+        operatorBalances[operator][erc20][blockHash] = balance;
+    }
+}
